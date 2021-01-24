7e9f670b429c3c3c90a1695894cf1ba0ffe6fbca
kaustubhgithubTRY <74052245+kaustubhgithubTRY@users.noreply.github.com>
2021-01-24 15:57:34 +0530

    Add files via upload

---
+++ C=31/AngryBirdsStage6-master/Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeimage =loadImage("sprites/smoke.png");
+    this.wall=[];
+  }
+
+  display() {
+    super.display();
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+   if(this.body.position.x>200 && this.body.velocity.x >10){
+    var pos=[this.body.position.x,this.body.position.y];
+    this.wall.push(pos)
+    
+   }
+
+    for(var i = 0;i<this.wall.length;i++){
+      image(this.smokeimage,this.wall[i][0],this.wall[i][1])
+    }
+    
+  }
+}
